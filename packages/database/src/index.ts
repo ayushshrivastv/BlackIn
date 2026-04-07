932512e58f679e349cee0923140dbd8d889c1bd7
Ayush Srivastava <ayush.srivastav@icloud.com>
2026-04-07 16:55:00 +0530

    Add shared database client exports and initialize the types package

---
+++ packages/database/src/index.ts
@@ -0,0 +1,7 @@
+/*
+ * Lighthouse
+ * © 2026 ayushshrivastv
+ */
+
+export { prisma } from "./client"; // exports instance of prisma
+export * from "../generated/client"; // exports generated types from prisma
